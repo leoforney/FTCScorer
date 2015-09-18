a91128625632d59c9fdcfc5536303140e1914173
leoforney <forneyleo@gmail.com>
2015-09-17 22:52:11 -0500

    Add WebView for scoring page, can draw on now.

--- app/src/main/java/tk/leoforney/ftcscorer/MainActivity.java
+++ app/src/main/java/tk/leoforney/ftcscorer/MainActivity.java
@@ -1,16 +1,62 @@
 package tk.leoforney.ftcscorer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.Writer;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView htmlWebView = (WebView)findViewById(R.id.fieldPad);
+        WebSettings webSetting = htmlWebView.getSettings();
+        webSetting.setJavaScriptEnabled(true);
+        webSetting.setDisplayZoomControls(true);
+        htmlWebView.setWebChromeClient(new WebChromeClient());
+
+        String htmlFilename = "index.html";
+        AssetManager mgr = getBaseContext().getAssets();
+        try {
+            InputStream in = mgr.open(htmlFilename, AssetManager.ACCESS_BUFFER);
+            String htmlContentInStringFormat = StreamToString(in);
+            in.close();
+            htmlWebView.loadDataWithBaseURL(null, htmlContentInStringFormat, "text/html", "utf-8", null);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String StreamToString(InputStream in) throws IOException {
+        if(in == null) {
+            return "";
+        }
+        Writer writer = new StringWriter();
+        char[] buffer = new char[1024];
+        try {
+            Reader reader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            int n;
+            while ((n = reader.read(buffer)) != -1) {
+                writer.write(buffer, 0, n);
+            }
+        } finally {
+        }
+        return writer.toString();
     }
 
     @Override
